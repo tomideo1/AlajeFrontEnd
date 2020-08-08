fee6d426ecc9d8535ffab10395bfa5e29323b57d
tomideo1 <ayotomideaina@gmail.com>
2020-08-08 15:01:53 +0100

    sentry integration

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router/router";
 import store from "./store";
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
 Vue.config.productionTip = false;
+
+Sentry.init({
+  dsn: "https://b4353ab1e01e4d4bb7a20e9c9f1a0b31@o430929.ingest.sentry.io/5383067",
+  integrations: [new VueIntegration({ Vue, attachProps: true })]
+});
 
 new Vue({
   router,
